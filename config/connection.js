97d60ced1f748a470df94cd367ee94cb3d0edc73
dannikaml <dannikaml@hotmail.com>
2023-04-27 11:52:12 -0500

    fixed connection for server and mongodb/mongoose

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,8 @@
-const { connection } = require('mongoose');
+const { connect, connection } = require('mongoose');
 
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost/socially';
+
+connect(connectionString);
 
 module.exports = connection;
